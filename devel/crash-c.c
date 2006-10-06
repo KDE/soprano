488259778194e7aef2dcd459c6c7cfa751aa146f
Daniele Galdi <daniele.galdi@gmail.com>
2006-10-06 15:34:56 +0000

    Make englishbreakfastnetwork happy
    svn path=/trunk/playground/base/qrdf/; revision=593032

--- devel/crash-c.c
+++ devel/crash-c.c
@@ -1,3 +1,22 @@
+/* This file is part of QRDF
+ *
+ * Copyright (C) 2006 Duncan Mac-Vicar <duncan@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -12,3 +31,4 @@ void main(int argc, char *argv[])
   librdf_world_open(world);
   librdf_free_world(world);
 }
+
