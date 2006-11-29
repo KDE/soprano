156b437309e95c21f45555b4f858785639973080
Daniele Galdi <daniele.galdi@gmail.com>
2006-11-29 17:57:53 +0000

    Prepare soprano to disable Nepomuk backend.
    svn path=/trunk/playground/base/qrdf/; revision=609184

--- cmake/modules/FindKNep.cmake
+++ cmake/modules/FindKNep.cmake
@@ -1,6 +1,6 @@
-INCLUDE(CheckIncludeFiles)
+include(CheckIncludeFiles)
 
-check_include_files(knep/knep.h HAVE_KNEP_KNEP_H)
+check_include_files("knep/knep.h;/home/kdedev/test/include/knep/knep.h" HAVE_KNEP_KNEP_H)
 
 if (HAVE_KNEP_KNEP_H)
    set(KNEP_HEADERS_FOUND TRUE)
@@ -16,8 +16,9 @@ endif (KNEP_HEADERS_FOUND)
 
 if (KNEP_HEADERS_FOUND)
    set(KNEP_FOUND TRUE)
-   set(KNEP_LIBS ${ACL_LIBS})
+   set(KNEP_LIBS ${KNEP_LIBS})
    message(STATUS "Found KNepomuk: ${KNEP_LIBS}")
 endif (KNEP_HEADERS_FOUND)
 
 #MARK_AS_ADVANCED(KNEP_LIBS)
+CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/config.h.cmake ${CMAKE_CURRENT_BINARY_DIR}/config.h)
---
+++ soprano/Config.h.cmake
@@ -0,0 +1,1 @@
+#cmakedefine HAVE_NEPOMUK_H 1
