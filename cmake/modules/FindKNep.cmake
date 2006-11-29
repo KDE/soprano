e3aedac16dd4a0a9a555297505c3b633e61ebfdb
Volker Krause <vkrause@kde.org>
2006-11-29 21:36:00 +0000

    find nepomuk for me as well and build with and without it
    svn path=/trunk/playground/base/qrdf/; revision=609247

--- cmake/modules/FindKNep.cmake
+++ cmake/modules/FindKNep.cmake
@@ -2,13 +2,17 @@ FIND_PATH(KNEP_INCLUDE_DIR knep/knep.h
    /usr/include
    /usr/local/include
    /home/kdedev/test/include
+   ${KDE4_INCLUDE_DIR}
+   ${INCLUDE_INSTALL_DIR}
 )
 
 FIND_LIBRARY(KNEP_LIBRARIES NAMES knepclient
    PATHS
    /usr/lib
    /usr/local/lib
-   /home/kdedev/test/lib  
+   /home/kdedev/test/lib
+   ${KDE4_LIB_DIR}
+   ${LIB_INSTALL_DIR}
 )
 
 if(KNEP_INCLUDE_DIR AND KNEP_LIBRARIES)
