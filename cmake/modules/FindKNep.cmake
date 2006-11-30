5a56846e1cbf92c607b4acf68cad70b5187f55d0
Daniele Galdi <daniele.galdi@gmail.com>
2006-11-30 13:32:57 +0000

    Remove hard coded path
    svn path=/trunk/playground/base/qrdf/; revision=609374

--- cmake/modules/FindKNep.cmake
+++ cmake/modules/FindKNep.cmake
@@ -1,7 +1,6 @@
 FIND_PATH(KNEP_INCLUDE_DIR knep/knep.h
    /usr/include
    /usr/local/include
-   /home/kdedev/test/include
    ${KDE4_INCLUDE_DIR}
    ${INCLUDE_INSTALL_DIR}
 )
@@ -10,7 +9,6 @@ FIND_LIBRARY(KNEP_LIBRARIES NAMES knepclient
    PATHS
    /usr/lib
    /usr/local/lib
-   /home/kdedev/test/lib
    ${KDE4_LIB_DIR}
    ${LIB_INSTALL_DIR}
 )
