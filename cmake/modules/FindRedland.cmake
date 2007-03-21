4e9c1cef995276bf2e5b6b67b0ce3ba38ae7d69e
Sebastian Trueg <sebastian@trueg.de>
2007-03-21 10:21:11 +0000

    Fixed the FindRedland macro to make it work on Win32
    svn path=/trunk/kdereview/qrdf/; revision=644937

--- cmake/modules/FindRedland.cmake
+++ cmake/modules/FindRedland.cmake
@@ -1,26 +1,60 @@
-FIND_PROGRAM(REDLANDCONFIG_EXECUTABLE NAMES redland-config PATHS
-   /usr/bin
-   /usr/local/bin
-)
-#reset vars
-set(REDLAND_LIBRARIES)
-set(REDLAND_CFLAGS)
+# - Try to find Redland
+# Once done this will define
+#
+#  REDLAND_FOUND       - system has Redland
+#  REDLAND_LIBRARIES   - Link these to use REDLAND
+#  REDLAND_DEFINITIONS - Compiler switches required for using REDLAND
 
-# if redland-config has been found
-IF(REDLANDCONFIG_EXECUTABLE)
+# (c) 2007 Sebastian Trueg <trueg@kde.org>
+#
+# Based on FindFontconfig Copyright (c) 2006, Laurent Montel, <montel@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-  EXEC_PROGRAM(${REDLANDCONFIG_EXECUTABLE} ARGS --libs RETURN_VALUE _return_VALUE OUTPUT_VARIABLE REDLAND_LIBRARIES)
 
-  EXEC_PROGRAM(${REDLANDCONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE REDLAND_CFLAGS)
+if (REDLAND_LIBRARIES AND REDLAND_INCLUDE_DIR)
 
-  IF(REDLAND_LIBRARIES AND REDLAND_CFLAGS)
-    SET(Redland_FOUND TRUE)
-    message(STATUS "Found Redland: ${REDLAND_LIBRARIES}")
-  ENDIF(REDLAND_LIBRARIES AND REDLAND_CFLAGS)
+  # in cache already
+  set(REDLAND_FOUND TRUE)
 
-  MARK_AS_ADVANCED(REDLAND_CFLAGS REDLAND_LIBRARIES)
+else (REDLAND_LIBRARIES AND REDLAND_INCLUDE_DIR)
 
-ELSE(REDLANDCONFIG_EXECUTABLE)
-   SET(Redland_FOUND FALSE)
-   message(FATAL_ERROR "Redland library not found") 
-ENDIF(REDLANDCONFIG_EXECUTABLE)
+  if (NOT WIN32)
+    # use pkg-config to get the directories and then use these values
+    # in the FIND_PATH() and FIND_LIBRARY() calls
+    include(UsePkgConfig)
+
+    pkgconfig(redland _REDLANDIncDir _REDLANDLinkDir _REDLANDLinkFlags _REDLANDCflags)
+
+    set(REDLAND_DEFINITIONS ${_REDLANDCflags} CACHE INTERNAL "The compilation flags for redland")
+  endif (NOT WIN32)
+
+  find_path(REDLAND_INCLUDE_DIR redland.h
+    PATHS
+    ${_REDLANDIncDir}
+    /usr/X11/include
+  )
+
+  find_library(REDLAND_LIBRARIES NAMES rdf
+    PATHS
+    ${_REDLANDLinkDir}
+  )
+
+  if (REDLAND_LIBRARIES AND REDLAND_INCLUDE_DIR)
+     set(REDLAND_FOUND TRUE)
+  endif (REDLAND_LIBRARIES AND REDLAND_INCLUDE_DIR)
+
+  if (REDLAND_FOUND)
+    if (NOT REDLAND_FIND_QUIETLY)
+      message(STATUS "Found REDLAND: ${REDLAND_LIBRARIES}")
+    endif (NOT REDLAND_FIND_QUIETLY)
+  else (REDLAND_FOUND)
+    if (REDLAND_FIND_REQUIRED)
+      message(FATAL_ERROR "Could NOT find REDLAND")
+    endif (REDLAND_FIND_REQUIRED)
+  endif (REDLAND_FOUND)
+
+#  mark_as_advanced(REDLAND_LIBRARIES)
+
+endif (REDLAND_LIBRARIES AND REDLAND_INCLUDE_DIR)
