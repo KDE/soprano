27153bdde45685c816a72292b5ea736c39c3ef2c
Daniele Galdi <daniele.galdi@gmail.com>
2007-08-01 11:50:39 +0000

    Added cached condition
    svn path=/branches/work/soprano2/; revision=695118

--- cmake/modules/FindMySQL.cmake
+++ cmake/modules/FindMySQL.cmake
@@ -5,18 +5,27 @@
 #  MYSQL_LIBRARIES, the libraries needed to use MySQL.
 #  MYSQL_FOUND, If false, do not try to use MySQL.
 
-find_file(MYSQL_CONFIG mysql_config)
+if ( MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES )
 
-if(MYSQL_CONFIG)
-    set(MYSQL_FOUND TRUE)
+  # in cache already
+  message(STATUS "Found Mysql: ${MYSQL_LIBRARIES} [${MYSQL_INCLUDE_DIR}] (cached)")
+  set(TSTORE_FOUND TRUE)
 
-    execute_process(COMMAND ${MYSQL_CONFIG} --include OUTPUT_VARIABLE MYSQL_INCLUDE_DIR)
-    execute_process(COMMAND ${MYSQL_CONFIG} --libs OUTPUT_VARIABLE MYSQL_LIBRARIES)
+else ( MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES )
+    find_file(MYSQL_CONFIG mysql_config)
 
-    message(STATUS "MySQL found.")
-    message(STATUS "MySQL include dir: ${MYSQL_INCLUDE_DIR}")
-    message(STATUS "MySQL libs: ${MYSQL_LIBRARIES}")
-else(MYSQL_CONFIG)
-    set(MYSQL_FOUND FALSE)
-    message(STATUS "MySQL not found.")
-endif(MYSQL_CONFIG)
+    if (MYSQL_CONFIG)
+        set(MYSQL_FOUND TRUE)
+
+        execute_process(COMMAND ${MYSQL_CONFIG} --include OUTPUT_VARIABLE MYSQL_INCLUDE_DIR)
+        execute_process(COMMAND ${MYSQL_CONFIG} --libs OUTPUT_VARIABLE MYSQL_LIBRARIES)
+
+        message(STATUS "MySQL found.")
+        message(STATUS "MySQL include dir: ${MYSQL_INCLUDE_DIR}")
+        message(STATUS "MySQL libs: ${MYSQL_LIBRARIES}")
+    else (MYSQL_CONFIG)
+        set(MYSQL_FOUND FALSE)
+        message(STATUS "MySQL not found.")
+    endif (MYSQL_CONFIG)
+
+endif ( MYSQL_INCLUDE_DIR AND MYSQL_LIBRARIES )
