3910b7451b2fe551f8939b14fd960678ff811aa0
Matthew Woehlke <mw_triad@users.sourceforge.net>
2007-09-25 16:18:04 +0000

    actually use redland-config instead of just seeing that it is there and failing to find the libs
    svn path=/trunk/kdesupport/soprano/; revision=716925

--- cmake/modules/FindRedland.cmake
+++ cmake/modules/FindRedland.cmake
@@ -31,7 +31,51 @@
       COMMAND redland-config --version
       OUTPUT_VARIABLE redland_VERSION
       )
+
+    # extract include paths from redland-config
+    EXECUTE_PROCESS(
+      COMMAND redland-config --cflags
+      OUTPUT_VARIABLE redland_LIBS_ARGS)
+    STRING( REPLACE " " ";" redland_LIBS_ARGS ${redland_LIBS_ARGS} )
+    FOREACH( _ARG ${redland_LIBS_ARGS} )
+      IF(${_ARG} MATCHES "^-I")
+        STRING(REGEX REPLACE "^-I" "" _ARG ${_ARG})
+        LIST(APPEND redland_INCLUDE_DIRS ${_ARG})
+      ENDIF(${_ARG} MATCHES "^-I")
+    ENDFOREACH(_ARG)
+
+    # extract lib paths from redland-config
+    EXECUTE_PROCESS(
+      COMMAND redland-config --libs
+      OUTPUT_VARIABLE redland_CFLAGS_ARGS)
+    STRING( REPLACE " " ";" redland_CFLAGS_ARGS ${redland_CFLAGS_ARGS} )
+    FOREACH( _ARG ${redland_CFLAGS_ARGS} )
+      IF(${_ARG} MATCHES "^-L")
+        STRING(REGEX REPLACE "^-L" "" _ARG ${_ARG})
+        LIST(APPEND redland_LIBRARY_DIRS ${_ARG})
+      ENDIF(${_ARG} MATCHES "^-L")
+    ENDFOREACH(_ARG)
   endif(REDLAND_CONFIG)
+
+  # rasqal is not redland, look for it separately
+  FIND_PROGRAM(
+    RASQAL_CONFIG
+    NAMES rasqal-config
+    )
+
+  if(RASQAL_CONFIG)
+    # extract include paths from rasqal-config
+    EXECUTE_PROCESS(
+      COMMAND rasqal-config --cflags
+      OUTPUT_VARIABLE rasqal_CFLAGS_ARGS)
+    STRING( REPLACE " " ";" rasqal_CFLAGS_ARGS ${rasqal_CFLAGS_ARGS} )
+    FOREACH( _ARG ${rasqal_CFLAGS_ARGS} )
+      IF(${_ARG} MATCHES "^-I")
+        STRING(REGEX REPLACE "^-I" "" _ARG ${_ARG})
+        LIST(APPEND rasqal_INCLUDE_DIRS ${_ARG})
+      ENDIF(${_ARG} MATCHES "^-I")
+    ENDFOREACH(_ARG)
+  endif(RASQAL_CONFIG)
 
   # now a hack for win32 (only?)
   # soprano only includes <redland.h> instead <redland/redland.h>
@@ -39,7 +83,6 @@
     find_path(REDLAND_INCLUDE_DIR_TMP redland/redland.h
       PATHS
       ${_REDLANDIncDir}
-      /usr/X11/include
     )
     if(REDLAND_INCLUDE_DIR_TMP)
       set(REDLAND_INCLUDE_DIR ${REDLAND_INCLUDE_DIR_TMP}/redland CACHE PATH "Path to a file.")
@@ -48,7 +91,6 @@
     find_path(RASQAL_INCLUDE_DIR_TMP redland/rasqal.h
       PATHS
       ${redland_INCLUDE_DIRS}
-      /usr/X11/include
     )
     if(RASQAL_INCLUDE_DIR_TMP)
       set(RASQAL_INCLUDE_DIR ${RASQAL_INCLUDE_DIR_TMP}/redland CACHE PATH "Path to a file.")
@@ -62,6 +104,7 @@
     find_path(RASQAL_INCLUDE_DIR rasqal.h
       PATHS
       ${redland_INCLUDE_DIRS}
+      ${rasqal_INCLUDE_DIRS}
       /usr/X11/include
     )
   endif(WIN32)
@@ -70,6 +113,9 @@
     PATHS
     ${redland_LIBRARY_DIRS}
   )
+  MESSAGE(STATUS "redland libs: ${REDLAND_LIBRARIES}")
+  MESSAGE(STATUS "redland includes: ${REDLAND_INCLUDE_DIR}")
+  MESSAGE(STATUS "rasqal includes: ${RASQAL_INCLUDE_DIR}")
 
   if (REDLAND_LIBRARIES AND REDLAND_INCLUDE_DIR AND RASQAL_INCLUDE_DIR)
     set(REDLAND_FOUND TRUE)
