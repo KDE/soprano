d61083579188e4583a241b2c966efc6922011293
Sebastian Trueg <sebastian@trueg.de>
2008-03-20 16:51:06 +0000

    Updated sesame to 2.0.1
    svn path=/branches/soprano/2.0/; revision=788093

--- backends/sesame2/sesame2-config.h.cmake
+++ backends/sesame2/sesame2-config.h.cmake
@@ -1,1 +1,1 @@
-#define SESAME2_CLASSPATH "${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/openrdf-sesame-2.0-onejar.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-api-1.4.2.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-simple-1.4.2.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/"
+#define SESAME2_CLASSPATH "${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/openrdf-sesame-2.0.1-onejar.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-api-1.4.2.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-simple-1.4.2.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/"
