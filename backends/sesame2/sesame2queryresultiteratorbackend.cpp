2f98921ca0830ff6f8bf31a683cb190163676e8c
Sebastian Trueg <sebastian@trueg.de>
2008-04-17 16:23:27 +0000

    fixed ask queries
    svn path=/trunk/kdesupport/soprano/; revision=798145

--- backends/sesame2/sesame2queryresultiteratorbackend.cpp
+++ backends/sesame2/sesame2queryresultiteratorbackend.cpp
@@ -88,8 +88,6 @@ Soprano::Sesame2::QueryResultIteratorBackend::QueryResultIteratorBackend( bool r
     : d( new Private( result ) )
 {
     d->model = model;
-    // there is no need for us blocking the model
-    close();
 }
 
 
