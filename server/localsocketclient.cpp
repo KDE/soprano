9a84226d13cfdb38ae8f014d49d912f00566a535
Jarosław Staniek <staniek@kde.org>
2008-04-30 14:46:51 +0000

    re-add nonconst isConnected() to avoid BIC change (e.g. improtant for kdepim deployments)
    svn path=/trunk/kdesupport/soprano/; revision=802772

--- server/localsocketclient.cpp
+++ server/localsocketclient.cpp
@@ -103,6 +103,11 @@ bool Soprano::Client::LocalSocketClient::isConnected() const
     return d->socket.isValid();
 }
 
+bool Soprano::Client::LocalSocketClient::isConnected()
+{
+    return d->socket.isValid();
+}
+
 
 void Soprano::Client::LocalSocketClient::disconnect()
 {
