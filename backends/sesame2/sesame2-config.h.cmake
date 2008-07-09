47d9c5666f829098ae1fbc7fedc11c53ec420b0a
Sebastian Trueg <sebastian@trueg.de>
2008-07-09 12:42:02 +0000

    Updated slf4j to version 1.5.2
    svn path=/trunk/kdesupport/soprano/; revision=829931

--- backends/sesame2/sesame2-config.h.cmake
+++ backends/sesame2/sesame2-config.h.cmake
@@ -1,1 +1,1 @@
-#define SESAME2_CLASSPATH "${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/openrdf-sesame-2.1.2-onejar.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-api-1.5.0.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-simple-1.5.0.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/"
+#define SESAME2_CLASSPATH "${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/openrdf-sesame-2.1.2-onejar.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-api-1.5.2.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/slf4j-simple-1.5.2.jar:${CMAKE_INSTALL_PREFIX}/share/soprano/sesame2/"
