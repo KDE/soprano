913e2a89062d4636558b6a9d211ebfb6048d3a51
Sebastian Trueg <sebastian@trueg.de>
2008-12-16 10:41:08 +0000

    Fixed include protection
    svn path=/trunk/kdesupport/soprano/; revision=897517

--- soprano/util/simplenodeiterator.h
+++ soprano/util/simplenodeiterator.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of Soprano Project.
  *
- * Copyright (C) 2007 Sebastian Trueg <trueg@kde.org>
+ * Copyright (C) 2007-2008 Sebastian Trueg <trueg@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -19,8 +19,8 @@
  * Boston, MA 02110-1301, USA.
  */
 
-#ifndef _SOPRANO_SIMPLE_STATEMENT_ITERATOR_H_
-#define _SOPRANO_SIMPLE_STATEMENT_ITERATOR_H_
+#ifndef _SOPRANO_SIMPLE_NODE_ITERATOR_H_
+#define _SOPRANO_SIMPLE_NODE_ITERATOR_H_
 
 #include "nodeiterator.h"
 #include "soprano_export.h"
