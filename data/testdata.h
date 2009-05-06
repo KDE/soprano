fd90a13c0dadd3e9cd95bae540c7b7e9793c49e9
Sebastian Trueg <sebastian@trueg.de>
2009-05-06 11:14:28 +0000

    rdfs:label and rdfs:comment now use plain literals
    svn path=/trunk/kdesupport/soprano/; revision=964228

--- data/testdata.h
+++ data/testdata.h
@@ -8,58 +8,58 @@ static QList<Soprano::Statement> testData( bool withContext )
     Node galdiContext( QUrl( "http://soprano.sf.net/testdata/galdi" ) );
 
     sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/Soprano"),
-                   QUrl("http://www.w3.org/2000/01/rdf-schema#label"),
-                   LiteralValue("Soprano"),
-                   withContext ? mainContext : Node() ) );
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/Soprano"), 
-                   QUrl("http://www.w3.org/2000/01/rdf-schema#comment"), 
-                   LiteralValue("Soprano (formally known as QRDF) is a QT4 wrapper library for RDF storage, parsing, and querying"),
-                   withContext ? mainContext : Node() ) );
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/Soprano"), 
-                   QUrl("http://soprano.sf.net/test#author"), 
-                   QUrl("http://soprano.sf.net/testdata/DanieleGaldi"),
-                   withContext ? mainContext : Node() ) );
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/Soprano"), 
-                   QUrl("http://soprano.sf.net/test#author"), 
-                   QUrl("http://soprano.sf.net/testdata/SebastianTrueg"),
-                   withContext ? mainContext : Node() ) );
+                                   QUrl("http://www.w3.org/2000/01/rdf-schema#label"),
+                                   LiteralValue::createPlainLiteral("Soprano"),
+                                   withContext ? mainContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/Soprano"),
+                                   QUrl("http://www.w3.org/2000/01/rdf-schema#comment"),
+                                   LiteralValue::createPlainLiteral("Soprano (formally known as QRDF) is a QT4 wrapper library for RDF storage, parsing, and querying"),
+                                   withContext ? mainContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/Soprano"),
+                                   QUrl("http://soprano.sf.net/test#author"),
+                                   QUrl("http://soprano.sf.net/testdata/DanieleGaldi"),
+                                   withContext ? mainContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/Soprano"),
+                                   QUrl("http://soprano.sf.net/test#author"),
+                                   QUrl("http://soprano.sf.net/testdata/SebastianTrueg"),
+                                   withContext ? mainContext : Node() ) );
 
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/DanieleGaldi"), 
-                   QUrl("http://soprano.sf.net/test#name"), 
-                   LiteralValue("Daniele Galdi"),
-                   withContext ? galdiContext : Node() ) );
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/DanieleGaldi"), 
-                   QUrl("http://soprano.sf.net/test#email"), 
-                   LiteralValue("daniele.galdi@gmail.com"),
-                   withContext ? galdiContext : Node() ) );
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/DanieleGaldi"), 
-                   QUrl("http://soprano.sf.net/test#location"), 
-                   LiteralValue("Rome, Italy"),
-                   withContext ? galdiContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/DanieleGaldi"),
+                                   QUrl("http://soprano.sf.net/test#name"),
+                                   LiteralValue::createPlainLiteral("Daniele Galdi"),
+                                   withContext ? galdiContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/DanieleGaldi"),
+                                   QUrl("http://soprano.sf.net/test#email"),
+                                   LiteralValue::createPlainLiteral("daniele.galdi@gmail.com"),
+                                   withContext ? galdiContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/DanieleGaldi"),
+                                   QUrl("http://soprano.sf.net/test#location"),
+                                   LiteralValue::createPlainLiteral("Rome, Italy"),
+                                   withContext ? galdiContext : Node() ) );
 
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/SebastianTrueg"), 
-                   QUrl("http://soprano.sf.net/test#name"), 
-                   LiteralValue("Sebastian Trueg"),
-                   withContext ? truegContext : Node() ) );
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/SebastianTrueg"), 
-                   QUrl("http://soprano.sf.net/test#email"), 
-                   LiteralValue("trueg@kde.org"),
-                   withContext ? truegContext : Node() ) );
-    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/SebastianTrueg"), 
-                   QUrl("http://soprano.sf.net/test#location"), 
-                   LiteralValue("Freiburg, Germany"),
-                   withContext ? truegContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/SebastianTrueg"),
+                                   QUrl("http://soprano.sf.net/test#name"),
+                                   LiteralValue::createPlainLiteral("Sebastian Trueg"),
+                                   withContext ? truegContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/SebastianTrueg"),
+                                   QUrl("http://soprano.sf.net/test#email"),
+                                   LiteralValue::createPlainLiteral("trueg@kde.org"),
+                                   withContext ? truegContext : Node() ) );
+    sl.append( Soprano::Statement( QUrl("http://soprano.sf.net/testdata/SebastianTrueg"),
+                                   QUrl("http://soprano.sf.net/test#location"),
+                                   LiteralValue::createPlainLiteral("Freiburg, Germany"),
+                                   withContext ? truegContext : Node() ) );
 
     if( withContext ) {
-    sl.append( Statement( mainContext, 
-                  Soprano::Vocabulary::RDF::type(),
-                  Soprano::Vocabulary::NRL::GraphMetadata() ) );
-    sl.append( Statement( truegContext, 
-                  Soprano::Vocabulary::RDF::type(),
-                  Soprano::Vocabulary::NRL::GraphMetadata() ) );
-    sl.append( Statement( galdiContext, 
-                  Soprano::Vocabulary::RDF::type(),
-                  Soprano::Vocabulary::NRL::GraphMetadata() ) );
+        sl.append( Statement( mainContext,
+                              Soprano::Vocabulary::RDF::type(),
+                              Soprano::Vocabulary::NRL::GraphMetadata() ) );
+        sl.append( Statement( truegContext,
+                              Soprano::Vocabulary::RDF::type(),
+                              Soprano::Vocabulary::NRL::GraphMetadata() ) );
+        sl.append( Statement( galdiContext,
+                              Soprano::Vocabulary::RDF::type(),
+                              Soprano::Vocabulary::NRL::GraphMetadata() ) );
     }
 
     return sl;
