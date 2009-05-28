787533a73ab9aaa79ec97bcc98d18cccbbbf18a4
Sebastian Trueg <sebastian@trueg.de>
2009-05-28 07:45:16 +0000

    added URL
    svn path=/trunk/kdesupport/soprano/; revision=973912

--- soprano.pc.cmake
+++ soprano.pc.cmake
@@ -5,6 +5,7 @@ includedir=@INCLUDE_INSTALL_DIR@
 
 Name: Soprano
 Description: Powerful RDF storage, parsing, and serialization framework
+URL=http://soprano.sourceforge.net/
 Requires: QtCore
 Version: @CMAKE_SOPRANO_VERSION_STRING@
 Libs: -L@LIB_DESTINATION@ -lsoprano
