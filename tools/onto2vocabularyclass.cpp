782167c5b03aa1ff16608e695615adb5dfb3e3c4
Laurent Montel <montel@kde.org>
2009-08-01 09:47:20 +0000

    Make more explicite error.
    svn path=/trunk/kdesupport/soprano/; revision=1005497

--- tools/onto2vocabularyclass.cpp
+++ tools/onto2vocabularyclass.cpp
@@ -240,7 +240,7 @@ int main( int argc, char *argv[] )
     Model* model = Soprano::createModel( BackendSettings() << BackendSetting( BackendOptionStorageMemory ) );
     if ( !model ) {
         QTextStream s( stderr );
-        s << "Failed to create memory model" << endl;
+        s << "Failed to create memory model. Backend sesame2 not found" << endl;
         return 1;
     }
     while ( it.next() ) {
