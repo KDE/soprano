36b08c923df7b81c36d4198df9a81f8936c5c255
Pino Toscano <pino@kde.org>
2009-12-16 18:22:58 +0000

    avoid dlopen'ing static libs
    svn path=/trunk/kdesupport/soprano/; revision=1063018

--- cmake/modules/CheckLibraryLoads.c
+++ cmake/modules/CheckLibraryLoads.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <dlfcn.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
 	int i;
 	for (i = 1; i < argc; ++i) {
-		void* handle = dlopen(argv[i], RTLD_NOW);
+		const char* arg = argv[i];
+		size_t l = strlen(arg);
+		if (l >= 3) {
+			// skip static libs (ie ending with '.a')
+			if (arg[l - 2] == '.' && arg[l - 1] == 'a') {
+				continue;
+			}
+		}
+		void* handle = dlopen(arg, RTLD_NOW);
 		if (!handle) {
 			fprintf(stderr, "%s", dlerror());
 			return 1;
