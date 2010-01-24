9200914665647742d027a90632ca766b8e62e48d
Sebastian Trueg <sebastian@trueg.de>
2010-01-24 17:14:08 +0000

    * Updated copyright. * Use Graph instead of Model - thus, onto2vocabularyclass does not need a backend anymore.
    svn path=/trunk/kdesupport/soprano/; revision=1079627

--- tools/sopranocmd.cpp
+++ tools/sopranocmd.cpp
@@ -1,15 +1,22 @@
 /*
+ * This file is part of Soprano Project
  *
- * $Id: sourceheader 511311 2006-02-19 14:51:05Z trueg $
+ * Copyright (C) 2007-2010 Sebastian Trueg <trueg@kde.org>
  *
- * This file is part of the Soprano project.
- * Copyright (C) 2007-2009 Sebastian Trueg <trueg@kde.org>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * See the file "COPYING" for the exact licensing terms.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 #include <QtCore/QCoreApplication>
@@ -438,7 +445,7 @@ namespace {
     {
         QTextStream s( stdout );
         s << "sopranocmd " << Soprano::versionString() << endl;
-        s << "   Copyright (C) 2007-2009 Sebastian Trueg <trueg@kde.org>" << endl;
+        s << "   Copyright (C) 2007-2010 Sebastian Trueg <trueg@kde.org>" << endl;
         s << "   This program is free software; you can redistribute it and/or modify" << endl
           << "   it under the terms of the GNU General Public License as published by" << endl
           << "   the Free Software Foundation; either version 2 of the License, or" << endl
